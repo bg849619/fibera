1b6dd848ebfce5148faaf3d028ee98bef5014a07
Syax242 <73264562+Syax242@users.noreply.github.com>
2021-11-06 21:06:04 -0400

    Addition button and search functionality

--- Components/NetworkList.jsx
+++ Components/NetworkList.jsx
@@ -1,17 +1,25 @@
 import React, {Component} from 'react';
 class NetworkList extends Component{
     constructor(props){
-    super(props);
+        super(props);
+        this.state = {search:""};
+    }
+    handleChange(e){
+        this.setState({
+            search:e.target.value
+        })
     }
     render(){
         return(
             <div>
+                <button>+</button>
+                <input type="text" value={this.state.search} onChange={this.handleChange.bind(this)}/> 
                 <table>
                 <thead>
                 <tr><th>ID</th><th>Type</th><th>Name</th><th>Placement</th></tr>
                 </thead>
                 <tbody>
-                {this.props.networkComponents.map(networkComponent => (
+                {this.props.networkComponents.filter(component => (component.id.toString().includes(this.state.search) || (component.name && component.name.includes(this.state.search)))).map(networkComponent => (
                 <tr><td>{networkComponent.id}</td><td>{networkComponent.type}</td><td>{networkComponent.name}</td><td>{networkComponent.placement}</td></tr>))}
                 </tbody>
                 </table> 
