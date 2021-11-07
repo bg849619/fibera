366dacc161d4aaaf57aebc9d4329af77b13a0148
nickCunham <nc661320@ohio.edu>
2021-11-06 20:59:01 -0400

    added search bar

--- Components/Spec.jsx
+++ Components/Spec.jsx
@@ -1,18 +1,28 @@
 import React, {Component} from 'react';
 
 class Spec extends Component {
+
     constructor(props){
         super(props);
+        this.state = {search: ""};
     }
+
+    handleChange(e){
+        this.setState({
+            search: e.target.value
+        })
+    }
+
     render(){
         return (
             <div>
+                <input type="text" value={this.state.search} onChange={this.handleChange.bind(this)}/>
                 <table>
                     <thead>
                     <tr><th>Name</th><th>ID</th></tr>
                     </thead>
                     <tbody>
-                    {this.props.cableSpec.map(spec => (
+                    {this.props.cableSpec.filter(spec => (spec.name.includes(this.state.search) || (spec.name && spec.name.includes(this.state.search)))).map(spec => (
                     <tr><td>{spec.name}</td><td>{spec.id}</td></tr>))}
                     </tbody>
                 </table>
